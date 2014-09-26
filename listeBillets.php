37c2e0eb493c413c708565f2691b22ce0ab0bb59
gastonberger <julienvilvandre@yahoo.fr>
2014-09-26 14:56:37 +0200

    maj affichage

--- listeBillets.php
+++ listeBillets.php
@@ -1,13 +1,22 @@
 <?php $titre = 'Mon Blog' ?>
 
 <?php ob_start() ?>
+<?php
+
+    $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+                $requeteBillets = "SELECT * FROM t_billet";  // TODO
+                $billets = $bdd->query($requeteBillets) ?>
+
 <?php foreach ($billets as $billet): ?>
     <article>
-        <!-- TODO : affichage du billet -->
+            <h1 class="titreBillet"><?= $billet['BIL_TITRE'] ?></h1>
+            <time><?= $billet['BIL_DATE'] ?></time>
+            <p><?= $billet['BIL_CONTENU'] ?></p>
         <!-- TODO Bonus : affichage du nombre de commentaires -->
     </article>
     <hr />
 <?php endforeach; ?>
+<?php echo  $billets->rowCount(); ?>  
 <?php $contenu = ob_get_clean() ?>
 
 <?php include 'gabarit.php' ?>
